ea4780cc362605b06f05576f827d08be88d530aa
marjanfamili <marjanfamili@gmail.com>
2025-12-03 13:40:02 +0000

    preparing to use azula, movinf noise module to a seperate file, importing azula

--- src/auto_cast/processors/diffusion.py
+++ src/auto_cast/processors/diffusion.py
@@ -6,31 +6,14 @@ import torch.nn as nn
 from auto_cast.processors.base import Processor
 from auto_cast.types import Batch, EncodedBatch, RolloutOutput, Tensor
 
-
-class NoiseSchedule(nn.Module):
-    """Noise Schedule Module."""
-
-    def forward(self, t: Tensor) -> tuple[Tensor, Tensor]:
-        """Get alpha and sigma for given time steps t."""
-        msg = "Subclasses should implement this method."
-        raise NotImplementedError(msg)
-    
-
-class LogLinearSchedule(NoiseSchedule):
-    """Log-Linear Noise Schedule.
-    
-    Implements a log-linear schedule for alpha and sigma.
-    """
-
-    def __init__(self, sigma_min: float = 0.002, sigma_max: float = 80.0):
-        super().__init__()
-        self.log_sigma_min = math.log(sigma_min)
-        self.log_sigma_max = math.log(sigma_max)
-
-    def forward(self, t: Tensor) -> tuple[Tensor, Tensor]:
-        alpha = torch.ones_like(t)
-        sigma = torch.exp(self.log_sigma_min * (1 - t) + self.log_sigma_max * t)
-        return alpha, sigma
+from azula.noise import Schedule, VESchedule, VPSchedule, CosineSchedule, RectifiedSchedule
+from azula.denoise import (
+    Denoiser, 
+    SimpleDenoiser, 
+    KarrasDenoiser,
+    DiracPosterior,
+    GaussianPosterior
+)
 
 
 class DiffusionProcessor(Processor):

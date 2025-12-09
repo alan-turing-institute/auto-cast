487d407626c3576e3663192becf6cefff3e450cd
Paolo Conti <pconti@turing.ac.uk>
2025-12-09 09:35:35 +0000

    add ALL_METRICS

--- src/auto_cast/metrics/__init__.py
+++ src/auto_cast/metrics/__init__.py
@@ -1,3 +1,7 @@
-from .spatiotemporal import MSE, MAE, NMAE, NMSE, RMSE, NRMSE, VMSE, VRMSE, LInfinity
+from .spatiotemporal import MAE, MSE, NMAE, NMSE, NRMSE, RMSE, VMSE, VRMSE, LInfinity
 
-__all__ = ["MSE", "MAE", "NMAE", "NMSE", "RMSE", "NRMSE", "VMSE", "VRMSE", "LInfinity"]
+__all__ = ["MAE", "MSE", "NMAE", "NMSE", "NRMSE", "RMSE", "VMSE", "VRMSE", "LInfinity"]
+
+ALL_METRICS = (
+    MSE, MAE, NMAE, NMSE, RMSE, NRMSE, VMSE, VRMSE, LInfinity
+)

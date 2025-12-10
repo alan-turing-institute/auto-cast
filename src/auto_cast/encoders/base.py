cc3008999d969be7b886bcba682a1c42be1ac844
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-12-10 09:09:35 +0000

    Fix encode_batch

--- src/auto_cast/encoders/base.py
+++ src/auto_cast/encoders/base.py
@@ -59,11 +59,8 @@ class Encoder(nn.Module, ABC):
 
         # Assign output fields to inputs to be encoded identically in this default impl
         # Create a new batch with output fields as input fields to prevent mutation
-        output_batch = replace(
-            batch,
-            input_fields=batch.output_fields
-        )
-        
+        output_batch = replace(batch, input_fields=batch.output_fields)
+
         encoded_outputs = self.encode(output_batch)
 
         # Return encoded batch
